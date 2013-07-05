f1d6e9cb3019e1446fc22edf81955d71bcbc5e50
Greg Bergé <neoziro@gmail.com>
2013-07-05 22:25:26 +0200

    remove comments

--- config/socket.js
+++ config/socket.js
@@ -4,11 +4,6 @@ var socketIO = require('socket.io'),
 
 module.exports = function (server) {
 
-  // io : Socket io instance
-// questionProvider (callback) : A function that return the question in a callback
-// scoreComputer (solve, answer) : Return a score
-// pauseTime : Time between two questions
-
   // Game
   require('../server/game')({
     io: socketIO.listen(server),
@@ -25,36 +20,6 @@ module.exports = function (server) {
           time: 10
         };
       });
-      /*
-      var questions = [
-        {
-          id: 1,
-          question: 'Où se trouve Belgrade ?',
-          solve: {
-            lat: 44.80401,
-            long: 20.46513
-          },
-          time: 8
-        },
-        {
-          id: 2,
-          question: 'Où se trouve Paris ?',
-          solve: {
-            lat: 48.85341,
-            long: 2.3488
-          },
-          time: 5
-        },
-        {
-          id: 3,
-          question: 'Où se trouve Longyearbyen ?',
-          solve: {
-            lat: 78.2186,
-            long: 15.64007
-          },
-          time: 10
-        }
-      ];*/
 
       cb(questions[Math.floor(Math.random()*questions.length)]);
     },
